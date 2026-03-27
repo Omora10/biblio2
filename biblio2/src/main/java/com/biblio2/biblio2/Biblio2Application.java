7c4866558e128be0ac22adaeffd5a01df9c2750f
Orlando Mora <moradostin27@gmail.com>
2026-03-27 17:47:00 -0500

    primer commit biblio2

---
+++ biblio2/src/main/java/com/biblio2/biblio2/Biblio2Application.java
@@ -0,0 +1,13 @@
+package com.biblio2.biblio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biblio2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Biblio2Application.class, args);
+    }
+
+}
---
+++ biblio2/src/test/java/com/biblio2/biblio2/Biblio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblio2.biblio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biblio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
